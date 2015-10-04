3b40d2ed93f5e45929f5dc015b8bdfbdabd449e5
rchau05 <rnchau@mail.sfsu.edu>
2015-10-04 16:52:51 -0700

    added icons, added &ordm;

--- app.js
+++ app.js
@@ -3,74 +3,101 @@ angular.module('weatherApp',[])
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
    $scope.weather = [];
 
-    $scope.searchWeather = function () {
+    $scope.searchCity = function () {
       var city = $scope.city.replace(/\s+/, '');
-				var url = 'http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&callback=JSON_CALLBACK&q=' + city;
+       var url = 'http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&callback=JSON_CALLBACK&q=' + city;
       $http.jsonp(url)
         .then(function (response) {
-          console.log(response.data);
+        if (response == undefined || response == null){
+          alert("No City Found!");
+        } else {
           $scope.city = "";
           $scope.weather = response.data;
+          console.log($scope.weather)
+          }
+
+          //description of current weather
+    $scope.main = response.data.list[2].weather[0].description;
+console.log(response.data.list)
+          // set icon for current weather id
+          $scope.id = response.data.list[2].weather[0].id;
+          console.log($scope.id);
+            if ($scope.id >= 200 && $scope.id <= 299){
+              $scope.icon = "images/thunderstorm.png";
+            }if ($scope.id >= 300 && $scope.id <= 399){
+              $scope.icon = "images/drizzle.png";
+            }else if ($scope.id >= 500 && $scope.id <= 599){
+              $scope.icon = "images/rain.png";
+            }else if ($scope.id >= 600 && $scope.id <= 699){
+              $scope.icon = "images/snow.png";
+            }else if ($scope.id >= 700 && $scope.id <= 799){
+              $scope.icon = "images/atmosphere.png";
+            }else{ ($scope.id >= 800 && $scope.id <= 899)
+              $scope.icon = "images/clouds.png";
+            };
         });
     }
+
 }])
 
 .directive('currentWeather', function() {
-	return {
-		restrict: 'AE',
-		replace: true,
-		scope: {
-			city: '@'
-		},
-		templateUrl: 'templates/currentWeather.html',
-		controller: ['$scope', '$http',
-			function($scope, $http) {
-				var url = 'http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&callback=JSON_CALLBACK&q='
-				$scope.searchWeather = function(city) {
-					$http({ method: 'JSONP', url: url + city })
-					.then(function(response) {
-						$scope.weather = response.data;
-						console.log(response.data, $scope.weather.name);
-						$scope.city=''
-					});
-				}
-		}],
-		link: function (scope, element, attrs) {
-			scope.weather = scope.searchWeather(attrs.city);
-		}
-	}
+  return {
+    restrict: 'AE',
+    transclude: true,
+    scope: {
+      city: '@'
+    },
+    templateUrl: 'templates/currentWeather.html',
+    controller: ['$scope', '$http',
+      function($scope, $http) {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&callback=JSON_CALLBACK&q='
+        $scope.searchCity = function(city) {
+          $http({ method: 'JSONP', url: url + city })
+          // console.log('posted', city)
+          .success(function(data) {
+            $scope.weather = data;
+            console.log($scope.weather);
+            $scope.city=''
+          });
+        }
+    }],
+    link: function (scope, element, attrs) {
+      scope.weather = scope.searchCity(attrs.city);
+    }
+  }
 })
 
 .directive('nextThreeDayForecast', function() {
-	return {
-		restrict: 'AE',
-		scope: {
-			city: '@'
-		},
-		templateUrl: 'templates/nextThreeDayForecast.html',
-		controller: ['$scope', '$http', 
-		function($scope, $http) {
-			var url = "http://api.openweathermap.org/data/2.5/forecast?mode=json&units=imperial&callback=JSON_CALLBACK&q=";
-			$scope.searchWeather = function(city) {
-				$http({ method: 'JSONP', url: url + city })
-				.success(function(data) {
-					$scope.weather = data;
-				});
-				$scope.quantity = 3;
-			};
-			$scope.onceDaily = function(forecast) {
+  return {
+    restrict: 'AE',
+    transclude: true,
+    scope: {
+      city: '@'
+    },
+    templateUrl: 'templates/nextThreeDayForecast.html',
+    controller: ['$scope', '$http', 
+    function($scope, $http) {
+      var url = "http://api.openweathermap.org/data/2.5/forecast?mode=json&units=imperial&callback=JSON_CALLBACK&q=";
+      $scope.searchCity = function(city) {
+        $http({ method: 'JSONP', url: url + city })
+        .success(function(data) {
+          $scope.weather = data;
+        });
+        $scope.quantity = 3;
+      };
+      $scope.onceDaily = function(forecast) {
         return forecast.dt_txt.indexOf('09:00:00') > -1; 
       };
-		}],
-		link: function (scope, element, attrs) {
-			scope.weather = scope.searchWeather(attrs.city);
-		}
-	};
+    }],
+    link: function (scope, element, attrs) {
+      scope.weather = scope.searchCity(attrs.city);
+    }
+  };
 })
 
 //formats date to remove time
 .filter('formattedDate', function() {
-	return function (input) {
-		return input.substring(5, 10).replace(/-/, '/');
-	};
+  return function (input) {
+    return input.substring(5, 10).replace(/-/, '/');
+  };
 });

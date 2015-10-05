875fd6c4d7ca78ac42b199a2d09a1bde0aaa5fb5
rchau05 <rnchau@mail.sfsu.edu>
2015-10-05 02:35:07 -0700

    rendered icons

--- app.js
+++ app.js
@@ -15,28 +15,9 @@ angular.module('weatherApp',[])
           $scope.weather = response.data;
           console.log($scope.weather)
           }
-
-          //description of current weather
-    $scope.main = response.data.list[2].weather[0].description;
-console.log(response.data.list)
-          // set icon for current weather id
-          $scope.id = response.data.list[2].weather[0].id;
-          console.log($scope.id);
-            if ($scope.id >= 200 && $scope.id <= 299){
-              $scope.icon = "images/thunderstorm.png";
-            }if ($scope.id >= 300 && $scope.id <= 399){
-              $scope.icon = "images/drizzle.png";
-            }else if ($scope.id >= 500 && $scope.id <= 599){
-              $scope.icon = "images/rain.png";
-            }else if ($scope.id >= 600 && $scope.id <= 699){
-              $scope.icon = "images/snow.png";
-            }else if ($scope.id >= 700 && $scope.id <= 799){
-              $scope.icon = "images/atmosphere.png";
-            }else{ ($scope.id >= 800 && $scope.id <= 899)
-              $scope.icon = "images/clouds.png";
-            };
         });
     }
+    $scope.number ="123123"
 
 }])
 
@@ -53,11 +34,25 @@ console.log(response.data.list)
         var url = 'http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&callback=JSON_CALLBACK&q='
         $scope.searchCity = function(city) {
           $http({ method: 'JSONP', url: url + city })
-          // console.log('posted', city)
           .success(function(data) {
+            console.log(data)
             $scope.weather = data;
             console.log($scope.weather);
             $scope.city=''
+             $scope.id = data.weather[0].id;
+            if ($scope.id >= 200 && $scope.id <= 299){
+              $scope.icon = "thunderstorm.png";
+            }if ($scope.id >= 300 && $scope.id <= 399){
+              $scope.icon = "drizzle.png";
+            }else if ($scope.id >= 500 && $scope.id <= 599){
+              $scope.icon = "rain.png";
+            }else if ($scope.id >= 600 && $scope.id <= 699){
+              $scope.icon = "snow.png";
+            }else if ($scope.id >= 700 && $scope.id <= 799){
+              $scope.icon = "atmosphere.png";
+            }else{ ($scope.id >= 800 && $scope.id <= 899)
+              $scope.icon = "clouds.png";
+            };
           });
         }
     }],
@@ -82,6 +77,22 @@ console.log(response.data.list)
         $http({ method: 'JSONP', url: url + city })
         .success(function(data) {
           $scope.weather = data;
+          console.log($scope.weather);
+          $scope.city=''
+          $scope.id = data.list[0].weather[0].id;
+            if ($scope.id >= 200 && $scope.id <= 299){
+              $scope.icon = "thunderstorm.png";
+            }if ($scope.id >= 300 && $scope.id <= 399){
+              $scope.icon = "drizzle.png";
+            }else if ($scope.id >= 500 && $scope.id <= 599){
+              $scope.icon = "rain.png";
+            }else if ($scope.id >= 600 && $scope.id <= 699){
+              $scope.icon = "snow.png";
+            }else if ($scope.id >= 700 && $scope.id <= 799){
+              $scope.icon = "atmosphere.png";
+            }else{ ($scope.id >= 800 && $scope.id <= 899)
+              $scope.icon = "clouds.png";
+            };
         });
         $scope.quantity = 3;
       };
